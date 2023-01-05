e4e62696ab5473b35d293c35f6a0f47aab2301da
Christian <onyekachristian14@gmail.com>
2023-01-05 06:53:03 +0100

    fixed amount issue to have a fraction of 2 decimal place

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -43,7 +43,8 @@ class TransactionList extends StatelessWidget {
                       child: Padding(
                         padding: const EdgeInsets.all(6.0),
                         child: FittedBox(
-                          child: Text('\$ ${transactions[index].amount}'),
+                          child: Text(
+                              '\$ ${transactions[index].amount.toStringAsFixed(2)}'),
                         ),
                       ),
                     ),
