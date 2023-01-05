47de70db71fe5272b5c67268bc3d548a7803d178
Christian <onyekachristian14@gmail.com>
2023-01-05 17:31:05 +0100

    added dynamic height using Layout Builder

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -8,64 +8,61 @@ class TransactionList extends StatelessWidget {
   TransactionList(this.transactions, this.deleteTransaction);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 500,
-      child: transactions.isEmpty
-          ? Column(
-              children: [
-                Container(
-                  height: 200,
-                  child: Image.asset(
-                    "assets/images/no-transactions.png",
-                    fit: BoxFit.cover,
-                  ),
+    return transactions.isEmpty
+        ? Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.4,
+                child: Image.asset(
+                  "assets/images/no-transactions.png",
+                  fit: BoxFit.cover,
                 ),
-                SizedBox(
-                  height: 10,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "No Transaction Yet!",
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ],
+          )
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              return Card(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 5,
                 ),
-                Text(
-                  "No Transaction Yet!",
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-              ],
-            )
-          : ListView.builder(
-              itemBuilder: (ctx, index) {
-                return Card(
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
-                  ),
-                  color: Colors.white,
-                  elevation: 5,
-                  child: ListTile(
-                      leading: CircleAvatar(
-                        radius: 30,
-                        child: Padding(
-                          padding: const EdgeInsets.all(6.0),
-                          child: FittedBox(
-                            child: Text(
-                                '\$ ${transactions[index].amount.toStringAsFixed(2)}'),
-                          ),
+                color: Colors.white,
+                elevation: 5,
+                child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: FittedBox(
+                          child: Text(
+                              '\$ ${transactions[index].amount.toStringAsFixed(2)}'),
                         ),
                       ),
-                      title: Text(
-                        transactions[index].title,
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                      subtitle: Text(
-                        DateFormat.yMMMd().format(transactions[index].date),
-                      ),
-                      trailing: IconButton(
-                        icon: Icon(Icons.delete),
-                        color: Theme.of(context).errorColor,
-                        onPressed: () =>
-                            deleteTransaction(transactions[index].id),
-                      )),
-                );
-              },
-              itemCount: transactions.length,
-            ),
-    );
+                    ),
+                    title: Text(
+                      transactions[index].title,
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      color: Theme.of(context).errorColor,
+                      onPressed: () =>
+                          deleteTransaction(transactions[index].id),
+                    )),
+              );
+            },
+            itemCount: transactions.length,
+          );
   }
 }
